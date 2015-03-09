10f08cf415b3281b47b864bf682a1c8faa5ce784
Jack Cherng <jfcherng@gmail.com>
2015-03-10 02:13:04 +0800

    Now work fine under Windows.

--- linter.py
+++ linter.py
@@ -3,12 +3,13 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Jack Cherng
-# Copyright (c) 2013 jfcherng
+# Copyright (c) 2015 jfcherng
 #
 # License: MIT
 #
 
 from SublimeLinter.lint import Linter, util
+import platform
 
 
 class Iverilog (Linter):
@@ -16,9 +17,18 @@ class Iverilog (Linter):
     cmd = 'iverilog -t null'
     tempfile_suffix = 'verilog'
     # We are missing out on some errors by ignoring multiline messages.
-    regex = (
-        r'^([^:]+):.*:(?P<line>\d*):'
-        r'.((?P<error>error)|(?P<warning>warning))?'
-        r'(?P<message>.*)'
-    )
+
+    if platform.system() == 'Windows':
+        regex = (
+            r'^([^:]+):.*:(?P<line>\d*):'
+            r'.((?P<error>error)|(?P<warning>warning))?'
+            r'(?P<message>.*)'
+        )
+    else:
+        regex = (
+            r'^([^:]+):(?P<line>\d+): '
+            r'(?:(?P<error>error)|(?P<warning>warning): )?'
+            r'(?P<message>.+)'
+        )
+
     error_stream = util.STREAM_BOTH
