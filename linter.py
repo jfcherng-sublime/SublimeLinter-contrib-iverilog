4416b7ce97ccf6d9b1abab59cd5a404cf5bfe3e9
Jack Cherng <jfcherng@gmail.com>
2015-06-04 02:00:08 +0800

    Add match error msg pattern like "<file>:<line>: syntax error"
    This error msg is available in iverilog 0.10.0 (devel).

--- linter.py
+++ linter.py
@@ -3,8 +3,8 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Jack Cherng
-# Copyright (c) 2015 jfcherng
 # https://github.com/jfcherng/SublimeLinter-contrib-iverilog
+# Copyright (c) 2015 jfcherng
 #
 # License: MIT
 #
@@ -18,18 +18,20 @@ class Iverilog(Linter):
     syntax = ('verilog')
     cmd = 'iverilog -t null'
     tempfile_suffix = 'verilog'
+    multiline = False
     error_stream = util.STREAM_BOTH
 
-    # what kind of message should be caught?
+    # there is a ":" in the filepath under Windows
+    # like C:\SOME_FOLDERS\...\FILE
     if sublime.platform() == 'windows':
-        regex = (
-            r'^([^:]+):.*:(?P<line>\d*):'
-            r'.((?P<error>error)|(?P<warning>warning))?'
-            r'(?P<message>.*)'
-        )
+        filepath = r'[^:]+:[^:]+'
     else:
-        regex = (
-            r'^([^:]+):(?P<line>\d+): '
-            r'(?:(?P<error>error)|(?P<warning>warning): )?'
-            r'(?P<message>.+)'
-        )
+        filepath = r'[^:]+'
+
+    # what kind of message should be caught?
+    regex = (
+        r'(?P<file>{0}):(?P<line>\d+): '
+        r'((?P<warning>warning: )|(?P<error>error: |))'
+        r'(?P<message>.*)'
+        .format(filepath)
+    )
